30f1d76971aa54afd9dfc212a17c0160f054a771
Protokol999 <grisha.mitioglo.2003@mail.ru>
2024-03-31 20:31:54 +0300

    Add migrations

---
+++ JW.Infrastructure/Migrations/202403311727260_Katruk.cs
@@ -0,0 +1,125 @@
+﻿namespace JW.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Katruk : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.JewelryItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                        MaterialId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.Materials", t => t.MaterialId, cascadeDelete: true)
+                .Index(t => t.CategoryId)
+                .Index(t => t.MaterialId);
+            
+            CreateTable(
+                "dbo.Materials",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Reviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                        Rating = c.Int(nullable: false),
+                        JewelryItemId = c.Int(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .ForeignKey("dbo.JewelryItems", t => t.JewelryItemId, cascadeDelete: true)
+                .Index(t => t.JewelryItemId)
+                .Index(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        JewelryItemId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.JewelryItems", t => t.JewelryItemId, cascadeDelete: true)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.JewelryItemId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .Index(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderDetails", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Orders", "CustomerId", "dbo.Customers");
+            DropForeignKey("dbo.OrderDetails", "JewelryItemId", "dbo.JewelryItems");
+            DropForeignKey("dbo.Reviews", "JewelryItemId", "dbo.JewelryItems");
+            DropForeignKey("dbo.Reviews", "CustomerId", "dbo.Customers");
+            DropForeignKey("dbo.JewelryItems", "MaterialId", "dbo.Materials");
+            DropForeignKey("dbo.JewelryItems", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Orders", new[] { "CustomerId" });
+            DropIndex("dbo.OrderDetails", new[] { "JewelryItemId" });
+            DropIndex("dbo.OrderDetails", new[] { "OrderId" });
+            DropIndex("dbo.Reviews", new[] { "CustomerId" });
+            DropIndex("dbo.Reviews", new[] { "JewelryItemId" });
+            DropIndex("dbo.JewelryItems", new[] { "MaterialId" });
+            DropIndex("dbo.JewelryItems", new[] { "CategoryId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.Customers");
+            DropTable("dbo.Reviews");
+            DropTable("dbo.Materials");
+            DropTable("dbo.JewelryItems");
+            DropTable("dbo.Categories");
+        }
+    }
+}
