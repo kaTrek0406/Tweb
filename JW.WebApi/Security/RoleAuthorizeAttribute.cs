4be6568c1dec4b711600afac0f3ba46ed569009c
St1chFN <arseniy.vlad.00@gmail.com>
2024-05-13 21:06:19 +0300

    Change auth views

---
+++ JW.WebApi/Security/RoleAuthorizeAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace JW.WebApi.Security
+{
+    public class RoleAuthorizeAttribute : ActionFilterAttribute
+    {
+        private readonly string[] _roles;
+
+        public RoleAuthorizeAttribute(params string[] roles)
+        {
+            _roles = roles;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var httpContext = filterContext.HttpContext;
+
+            if (!httpContext.User.Identity.IsAuthenticated)
+            {
+                DenyAccess(filterContext);
+                return;
+            }
+
+            if (!_roles.Any(role => httpContext.User.IsInRole(role)))
+            {
+                DenyAccess(filterContext);
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        private void DenyAccess(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary
+                    {
+                        { "controller", "Error" },
+                        { "action", "Unauthorized" }
+                    });
+            }
+            else
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+    }
+}
