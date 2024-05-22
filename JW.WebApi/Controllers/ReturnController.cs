8546193d8e023a4aae5f40017228622788e13d50
St1chFN <arseniy.vlad.00@gmail.com>
2024-05-22 06:32:55 +0300

    Add return views

---
+++ JW.WebApi/Controllers/ReturnController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using JW.Domain;
+using JW.Infrastructure;
+
+namespace JW.Web.Controllers
+{
+    public class ReturnController : Controller
+    {
+        private readonly JewelryStoreContext _context;
+
+        public ReturnController()
+        {
+            _context = new JewelryStoreContext();
+        }
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View(new ReturnRequest());
+        }
+
+        [HttpPost]
+        public ActionResult SubmitReturn(ReturnRequest model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CustomerId = model.CustomerId;
+                model.ReturnDate = DateTime.Now;
+                _context.ReturnRequests.Add(model);
+                _context.SaveChanges();
+
+                return RedirectToAction("Confirmation");
+            }
+            return View("Form", model);
+        }
+
+        [HttpGet]
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
